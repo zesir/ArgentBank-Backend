f1c9f67a1cbc7982292e3dbb80f86a36bc1bca99
slarz <bourdinflorent86@gmail.com>
2026-03-30 14:03:56 +0200

    fix: remove nodemon import from tokenValidation

--- server/middleware/tokenValidation.js
+++ server/middleware/tokenValidation.js
@@ -1,25 +1,24 @@
-const jwt = require('jsonwebtoken')
-const { restart } = require('nodemon')
+const jwt = require("jsonwebtoken");
 
 module.exports.validateToken = (req, res, next) => {
-  let response = {}
+  let response = {};
 
   try {
     if (!req.headers.authorization) {
-      throw new Error('Token is missing from header')
+      throw new Error("Token is missing from header");
     }
 
-    const userToken = req.headers.authorization.split('Bearer')[1].trim()
+    const userToken = req.headers.authorization.split("Bearer")[1].trim();
     const decodedToken = jwt.verify(
       userToken,
-      process.env.SECRET_KEY || 'default-secret-key'
-    )
-    return next()
+      process.env.SECRET_KEY || "default-secret-key",
+    );
+    return next();
   } catch (error) {
-    console.error('Error in tokenValidation.js', error)
-    response.status = 401
-    response.message = error.message
+    console.error("Error in tokenValidation.js", error);
+    response.status = 401;
+    response.message = error.message;
   }
 
-  return res.status(response.status).send(response)
-}
+  return res.status(response.status).send(response);
+};
