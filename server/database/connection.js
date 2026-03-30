93d96acd0a9782d7ee8164d993dad489e38964bf
slarz <bourdinflorent86@gmail.com>
2026-03-30 12:12:44 +0200

    fix: load dotenv before other requires

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const mongoose = require("mongoose");
 
+const databaseUrl =
+  process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/argentBankDB"; // <-- IPv4 forcé
+console.log("Connecting to:", databaseUrl);
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // recommandé pour Mongoose > 5
+    });
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
